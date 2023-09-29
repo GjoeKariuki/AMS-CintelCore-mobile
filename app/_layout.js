f896fd849b5fb81e0ab9783a83dfa61fe39e0177
techsavant <ianwanjira4@gmail.com>
2023-09-29 10:13:19 +0300

    feat: imported footer in layout for global use

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,13 @@
 import { Slot } from "expo-router";
 import { Header } from "./components/header";
+import { Footer } from "./components/footer";
 
 export default function HomeLayout() {
   return (
     <>
       <Header />
       <Slot />
+      <Footer />
     </>
   );
 }
